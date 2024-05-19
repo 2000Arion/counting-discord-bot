56efb9c7bbf1db48ccf7e7290dbda4527bd53326
Arion2000 <github@arion2000.xyz>
2024-05-19 22:08:33 +0200

    Upload to v0.0.1

--- game/gameFunctions.js
+++ game/gameFunctions.js
@@ -24,22 +24,22 @@ async function initializeDatabase() {
 }
 
 async function getLatestCount() {
-    const [rows] = await pool.query('SELECT number FROM currentCount ORDER BY id DESC LIMIT 1');
+    const [rows] = await pool.query('SELECT number FROM CurrentCount ORDER BY id DESC LIMIT 1');
     return rows.length > 0 ? rows[0].number : 0;
 }
 
 async function getLatestSender() {
-    const [rows] = await pool.query('SELECT senderId FROM currentCount ORDER BY id DESC LIMIT 1');
+    const [rows] = await pool.query('SELECT senderId FROM CurrentCount ORDER BY id DESC LIMIT 1');
     return rows.length > 0 ? rows[0].senderId : null;
 }
 
 
 async function updateCount(newCount, senderId) {
-    await pool.query('INSERT INTO currentCount (number, senderId) VALUES (?, ?)', [newCount, senderId]);
+    await pool.query('INSERT INTO CurrentCount (number, senderId) VALUES (?, ?)', [newCount, senderId]);
 }
 
 async function getMode() {
-    const [rows] = await pool.query('SELECT mode FROM currentCount LIMIT 1');
+    const [rows] = await pool.query('SELECT mode FROM CurrentCount LIMIT 1');
     if (rows.length > 0) {
         return rows[0].mode;
     } else {
@@ -49,17 +49,17 @@ async function getMode() {
 
 async function resetCount(mode) {
     // Aktualisiere den Modus in der Datenbank
-    await pool.query('UPDATE currentCount SET mode = ?', [mode]);
+    await pool.query('UPDATE CurrentCount SET mode = ?', [mode]);
     // Generiere das Ziel basierend auf dem Modus
     const target = generateTarget(mode);
     // Setze den Zählstand auf 0
-    await pool.query('TRUNCATE TABLE currentCount');
+    await pool.query('TRUNCATE TABLE CurrentCount');
     // Füge das neue Ziel in die Datenbank ein
-    await pool.query('INSERT INTO currentCount (number, senderId, target, mode) VALUES (0, NULL, ?, ?)', [target, mode]);
+    await pool.query('INSERT INTO CurrentCount (number, senderId, target, mode) VALUES (0, NULL, ?, ?)', [target, mode]);
 }
 
 async function getTarget() {
-    const [rows] = await pool.query('SELECT target FROM currentCount ORDER BY id ASC LIMIT 1');
+    const [rows] = await pool.query('SELECT target FROM CurrentCount ORDER BY id ASC LIMIT 1');
     return rows.length > 0 ? rows[0].target : null;
 }
 
