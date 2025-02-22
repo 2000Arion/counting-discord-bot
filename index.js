0a2d17884e38f4159acd4b7598ae91b82140a2d5
Arion2000 <github@arion2000.xyz>
2025-02-22 17:08:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ client.on('messageCreate', async (message) => {
                     candidate++;
                 }
             case 'binary':
-                const binaryCount = await isValidBinary(latestCount) ? parseInt(latestCount, 2) : 0;
+                await const binaryCount = isValidBinary(latestCount) ? parseInt(latestCount, 2) : 0;
                 return binaryCount === 0 ? '1' : (binaryCount + 1).toString(2);
             default:
                 return latestCount + 1;
